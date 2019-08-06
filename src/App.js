d8a56390db36f1aeb542c7fc1730ee7c16ad78b6
manvirraul <manvirsinghraul@gmail.com>
2019-08-06 20:42:06 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Api_key = "e00eafb564c3e3c10afc20e113231972";
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      temperature:undefined,
+      city:undefined,
+      country:undefined,
+      humidity:undefined,
+      description:undefined,
+      error:undefined
+    }
+  }
+  getWeather = async e => {
+    
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const dataApi = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=e00eafb564c3e3c10afc20e113231972&units=metric`)
+    console.log(dataApi);
+    
+    if(city && country && dataApi.status===200){
+      const data = await dataApi.json();
+      this.setState({
+          temperature:data.main.temp,
+          city:data.name,
+          country:data.sys.country,
+          humidity:data.main.humidity,
+          description:data.weather[0].description,
+          Manvir:"manvir",
+          error:""
+        }) 
+        } else if(dataApi.status===404 && city && country){
+          this.setState({
+            temperature:undefined,
+            city:undefined,
+            country:undefined,
+            humidity:undefined,
+            description:undefined,
+            error:"Please enter correct city and country"
+          })
+        }else {
+          this.setState({
+            temperature:undefined,
+            city:undefined,
+            country:undefined,
+            humidity:undefined,
+            description:undefined,
+            error:"Please enter city and country"
+          })
+        }
+  };
+
+  render() {
+    
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles/>
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather data={this.state}/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+        
---
+++ src/components/weather.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+const Weather = (props) => (
+    <div className="weather__info">
+        {props.data.city && props.data.country && <p className="weather__key">Location    : <span className="weather__value"> {props.data.city} , {props.data.country}</span></p>}
+        {props.data.temperature && <p className="weather__key">Temperature : <span className="weather__value">{props.data.temperature}</span></p>}
+        {props.data.humidity && <p className="weather__key">Humidity    :<span className="weather__value">{props.data.humidity}</span></p>}
+        {props.data.description && <p className="weather__key">Description :<span className="weather__value">{props.data.description}</span></p>}
+        {props.data.error && <p className="weather__error">{props.data.error}</p>}
+    </div> 
+)
+
+ 
+export default Weather;
